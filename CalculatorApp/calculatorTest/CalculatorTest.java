89433b07b48a12e8fca6d39ec69f8662420214b8
AR1sUt3l <alecz08villagr@gmail.com>
2024-06-11 23:18:36 -0700

    Update CalculatorTest.java
    added more tests

--- CalculatorApp/calculatorTest/CalculatorTest.java
+++ CalculatorApp/calculatorTest/CalculatorTest.java
@@ -13,7 +13,11 @@ class CalculatorTest
 	void testSimpleOperations()
 	{
 		SimpleOperations calculator = new SimpleOperations();
-		assertEquals(3, calculator.addition(1, 2));
+		assertEquals(3, calculator.add(1, 2));
+		assertEquals(0.1, calculator.subtract(10, 9.9));
+		assertEquals(0, calculator.multiply(0, 999));
+		assertEquals(100000000, calculator.multiply(10000000, 10));
+		assertNotEquals(0, calculator.divide(1, 999999999));
 	}
 
 }
