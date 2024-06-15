f85b0da43313de67592584523a483d7375675a9b
AR1sUt3l <alecz08villagr@gmail.com>
2024-06-14 23:53:23 -0700

    Create MainView.java

---
+++ CalculatorApp/calculatorView/MainView.java
@@ -0,0 +1,20 @@
+package calculatorView;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainView extends JFrame
+{
+	private JPanel mainPanel = new JPanel();
+	private JPanel 
+	private JPanel resultsPanel = new JPanel();
+	private JPanel numbersPanel = new JPanel();
+	private JPanel operationsPanel = new JPanel();
+	
+	private J
+	
+	public MainView()
+	{
+		
+	}
+}
