9c6e8e5f439998512bf99a9d25caf87326daec2e
AR1sUt3l <alecz08villagr@gmail.com>
2024-06-18 21:08:50 -0700

    Update MainView.java
    add methods declarations to mainView

--- CalculatorApp/calculatorView/MainView.java
+++ CalculatorApp/calculatorView/MainView.java
@@ -1,19 +1,59 @@
 package calculatorView;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainView extends JFrame
 {
 	private JPanel mainPanel = new JPanel();
-	private JPanel 
+	private JPanel calculationPanel = new JPanel();
 	private JPanel resultsPanel = new JPanel();
 	private JPanel numbersPanel = new JPanel();
 	private JPanel operationsPanel = new JPanel();
 	
-	private J
-	
 	public MainView()
+	{
+		initialFrame();
+		initialUI();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	private void initialFrame()
+	{
+		setPreferredSize(new Dimension(350, 400));
+		setTitle("Calculator");
+	}
+	
+	private void initialUI()
+	{
+		addCalculation();
+		addResult();
+		addNumberButtons();
+		addOperationButtons();
+	}
+
+	private void addOperationButtons()
+	{
+		
+	}
+
+	private void addNumberButtons()
+	{
+		
+	}
+
+	private void addResult()
+	{
+		
+	}
+
+	private void addCalculation()
 	{
 		
 	}
