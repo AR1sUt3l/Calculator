4392568b7cc307f669f15ba8433e3287767383b9
AR1sUt3l <alecz08villagr@gmail.com>
2024-06-19 23:47:56 -0700

    Update MainView.java
    add buttons and listeners

--- CalculatorApp/calculatorView/MainView.java
+++ CalculatorApp/calculatorView/MainView.java
@@ -1,21 +1,47 @@
 package calculatorView;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MainView extends JFrame
 {
 	private JPanel mainPanel = new JPanel();
+	private JPanel topPanel = new JPanel();
 	private JPanel calculationPanel = new JPanel();
 	private JPanel resultsPanel = new JPanel();
 	private JPanel buttonsPanel = new JPanel();
 	private JPanel numbersPanel = new JPanel();
 	private JPanel operationsPanel = new JPanel();
+	
+	private JLabel working = new JLabel();
+	private JLabel results = new JLabel();
+	
+	private JButton plusButton = new JButton("+");
+	private JButton minusButton = new JButton("-");
+	private JButton timesButton = new JButton("x");
+	private JButton divideButton = new JButton("/");
+	
+	private JButton oneButton = new JButton("1");
+	private JButton twoButton = new JButton("2");
+	private JButton threeButton = new JButton("3");
+	private JButton fourButton = new JButton("4");
+	private JButton fiveButton = new JButton("5");
+	private JButton sixButton = new JButton("6");
+	private JButton sevenButton = new JButton("7");
+	private JButton eightButton = new JButton("8");
+	private JButton nineButton = new JButton("9");
+	private JButton zeroButton = new JButton("0");
+	private JButton decimalPointButton = new JButton(".");
+	private JButton equalButton = new JButton("=");
 	
 	public MainView()
 	{
@@ -37,56 +63,126 @@ public class MainView extends JFrame
 	private void initialUI()
 	{
 		mainPanel.setLayout(new GridLayout(3,1));
+		addWorking();
+		addButtons();
+		add(mainPanel);
+	}
+	
+	private void addWorking()
+	{
+		topPanel.setLayout(new BorderLayout());
 		addCalculation();
 		addResult();
-		addOperationButtons();
-		addNumberButtons();
-		add(mainPanel);
+		mainPanel.add(topPanel);
 	}
 
 	private void addCalculation()
 	{
 		calculationPanel.setBackground(Color.WHITE);
-		mainPanel.add(calculationPanel);
+		calculationPanel.add(working);
+		topPanel.add(calculationPanel, BorderLayout.CENTER);
 	}
 	
 	private void addResult()
 	{
 		resultsPanel.setBackground(Color.WHITE);
-		mainPanel.add(resultsPanel);
+		resultsPanel.add(results)
+;		topPanel.add(resultsPanel,BorderLayout.SOUTH);
+	}
+	
+	private void addButtons()
+	{
+		buttonsPanel.setLayout(new GridLayout(1,2));
+		addOperationButtons();
+		addNumberButtons();
+		mainPanel.add(buttonsPanel);
 	}
 	
 	private void addOperationButtons()
 	{
 		operationsPanel.setLayout(new GridLayout(2,2));
-		operationsPanel.add(new JButton("+"));
-		operationsPanel.add(new JButton("-"));
-		operationsPanel.add(new JButton("x"));
-		operationsPanel.add(new JButton("/"));
+		OperationButtonListener listener = new OperationButtonListener();
+		plusButton.addActionListener(listener);
+		minusButton.addActionListener(listener);
+		timesButton.addActionListener(listener);
+		divideButton.addActionListener(listener);
+		operationsPanel.add(plusButton);
+		operationsPanel.add(minusButton);
+		operationsPanel.add(timesButton);
+		operationsPanel.add(divideButton);
 		buttonsPanel.add(operationsPanel);
 	}
 	
 	private void addNumberButtons()
 	{
 		numbersPanel.setLayout(new GridLayout(4,3));
-		numbersPanel.add(new JButton("1"));
-		numbersPanel.add(new JButton("2"));
-		numbersPanel.add(new JButton("3"));
-		numbersPanel.add(new JButton("4"));
-		numbersPanel.add(new JButton("5"));
-		numbersPanel.add(new JButton("6"));
-		numbersPanel.add(new JButton("7"));
-		numbersPanel.add(new JButton("8"));
-		numbersPanel.add(new JButton("9"));
-		numbersPanel.add(new JButton("0"));
-		numbersPanel.add(new JButton("."));
-		numbersPanel.add(new JButton("="));
+		NumberButtonListener listener = new NumberButtonListener();
+		oneButton.addActionListener(listener);
+		twoButton.addActionListener(listener);
+		threeButton.addActionListener(listener);
+		fourButton.addActionListener(listener);
+		fiveButton.addActionListener(listener);
+		sixButton.addActionListener(listener);
+		sevenButton.addActionListener(listener);
+		eightButton.addActionListener(listener);
+		nineButton.addActionListener(listener);
+		zeroButton.addActionListener(listener);
+		decimalPointButton.addActionListener(listener);
+		numbersPanel.add(oneButton);
+		numbersPanel.add(twoButton);
+		numbersPanel.add(threeButton);
+		numbersPanel.add(fourButton);
+		numbersPanel.add(fiveButton);
+		numbersPanel.add(sixButton);
+		numbersPanel.add(sevenButton);
+		numbersPanel.add(eightButton);
+		numbersPanel.add(nineButton);
+		numbersPanel.add(zeroButton);
+		numbersPanel.add(decimalPointButton);
+		numbersPanel.add(equalButton);
 		buttonsPanel.add(numbersPanel);
-		mainPanel.add(buttonsPanel);
 	}
 	
-	private class NumberButtonListener extends ActionListener
+	private class NumberButtonListener implements ActionListener
 	{
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			String character = e.getActionCommand();
+			String currentWorking = working.getText();
+			currentWorking += character;
+			working.setText(currentWorking);
+			revalidate();
+		}
+		
+	}
+	
+	private class OperationButtonListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			String character = e.getActionCommand();
+			String currentWorking = working.getText();
+			currentWorking += " " + character + " ";
+			working.setText(currentWorking);
+			revalidate();
+		}
+		
+	}
+	
+	private class EqualButtonListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			String currentWorking = working.getText();
+			
+			
+		}
 		
 	}
 	
