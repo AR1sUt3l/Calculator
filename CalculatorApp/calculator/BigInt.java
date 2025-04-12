db1542836e59cb6fda1dfa17e9e9fec1140d4f80
Aleczandria <alecz08villagr@gmail.com>
2025-04-11 23:49:54 -0700

    Add constructor for BigInt class

--- CalculatorApp/calculator/BigInt.java
+++ CalculatorApp/calculator/BigInt.java
@@ -1,7 +1,17 @@
 package calculator;
+import java.util.Vector;
+import java.util.Arrays;
 
 public class BigInt
 {
+	Vector<Integer> number = new Vector<>();
 	
+	public BigInt(int[] newNumber)
+	{
+		for (int integer : newNumber)
+		{
+			number.add(integer);
+		}
+	}
 
 }
